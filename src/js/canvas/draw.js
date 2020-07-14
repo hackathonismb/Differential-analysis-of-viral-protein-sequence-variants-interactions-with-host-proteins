1739a1c6647c9a6da69c38078c38fc755227221d
mingzhang <redondoself@gmail.com>
2020-07-14 08:31:48 -0400

    lots of optimizations

--- src/js/canvas/draw.js
+++ src/js/canvas/draw.js
@@ -18,29 +18,31 @@ let getColor = colors();
  *   }
  * }
  */
-export function drawContactMap(canvas, data={x: [], y: [], data: {}}) {
+export function drawContactMap(canvas, data = {x: [], y: [], data: {}}) {
   let gridWidth = config.gridWidth;
+  // the size of the grid plus margins
   let w = gridWidth * (data.x.length + 1) + config.margin.left + config.margin.right;
   let h = gridWidth * (data.y.length + 1) + config.margin.top + config.margin.bottom;
   let ctx = initializeCanvas(canvas, {width: w, height: h});
-  ctx.margin = config.margin;
+  ctx.data = data;
+  ctx.highlighted = false;
 
   canvas.infoPanel = document.getElementById('info-panel');
 
   requestAnimationFrame(() => {
-    updateContactMap(ctx, data, true);
+    updateContactMap(ctx, true);
   });
 
   canvas.addEventListener('contextmenu', evt => {
     evt.preventDefault();
     let rect = canvas.getBoundingClientRect();
-    let x = evt.clientX - rect.left - ctx.margin.left + ctx.strokeWidth / 2;
-    let y = evt.clientY - rect.top - ctx.margin.top + ctx.strokeWidth / 2;
+    let x = evt.clientX - rect.left - config.margin.left + config.lineWidth / 2;
+    let y = evt.clientY - rect.top - config.margin.top + config.lineWidth / 2;
     let m = getIndex(x, w, gridWidth, config.circleRadius);
     let n = getIndex(y, h, gridWidth, config.circleRadius);
     let obj = data.data[`${data.x[m]}-${data.y[n]}`];
-    let flag = m > -1 && n > - 1 && obj.value;
-    if (flag) {
+    ctx.highlighted = m > -1 && n > -1 && obj.value;
+    if (ctx.highlighted) {
       canvas.infoPanel.classList.add('live');
       updateInfoPanel(canvas.infoPanel, obj, {left: evt.clientX, top: evt.clientY});
     }
@@ -49,7 +51,7 @@ export function drawContactMap(canvas, data={x: [], y: [], data: {}}) {
   canvas.addEventListener('mouseover', evt => {
     let rect = canvas.getBoundingClientRect();
     requestAnimationFrame(() => {
-      updateContactMap(ctx, data, false,{
+      updateContactMap(ctx, false, {
         x: evt.clientX - rect.left,
         y: evt.clientY - rect.top,
       });
@@ -59,7 +61,7 @@ export function drawContactMap(canvas, data={x: [], y: [], data: {}}) {
   canvas.addEventListener('mousemove', evt => {
     let rect = canvas.getBoundingClientRect();
     requestAnimationFrame(() => {
-      updateContactMap(ctx, data, true,{
+      updateContactMap(ctx, true, {
         x: evt.clientX - rect.left,
         y: evt.clientY - rect.top,
       });
@@ -68,24 +70,27 @@ export function drawContactMap(canvas, data={x: [], y: [], data: {}}) {
   });
   canvas.addEventListener('mouseleave', () => {
     requestAnimationFrame(() => {
-      updateContactMap(ctx, data, false);
+      updateContactMap(ctx, false);
       canvas.infoPanel.classList.remove('live');
     });
   });
 }
 
-function updateContactMap(ctx, data, required, pos={x: 0, y: 0}) {
+function updateContactMap(ctx, required, pos = {x: 0, y: 0}) {
+  let data = ctx.data;
+
   let gridWidth = config.gridWidth;
+  // the real size of the grid
   let w = gridWidth * (data.x.length + 1);
   let h = gridWidth * (data.y.length + 1);
 
-  let x = pos.x - ctx.margin.left + ctx.strokeWidth / 2;
-  let y = pos.y - ctx.margin.top + ctx.strokeWidth / 2;
+  let x = pos.x - config.margin.left + config.lineWidth / 2;
+  let y = pos.y - config.margin.top + config.lineWidth / 2;
   let m = getIndex(x, w, gridWidth, config.circleRadius);
   let n = getIndex(y, h, gridWidth, config.circleRadius);
-  let flag = m > -1 && n > - 1 && data.data[`${data.x[m]}-${data.y[n]}`].value;
+  ctx.highlighted = m > -1 && n > -1 && data.data[`${data.x[m]}-${data.y[n]}`].value;
 
-  if (!flag && !required) {
+  if (!ctx.highlighted && !required) {
     return;
   }
 
@@ -93,10 +98,9 @@ function updateContactMap(ctx, data, required, pos={x: 0, y: 0}) {
   ctx.clearRect(0, 0, ctx.w, ctx.h);
 
   ctx.strokeStyle = config.lineColor;
-  ctx.strokeWidth = config.lineWidth;
-  ctx.translate(ctx.margin.left - ctx.strokeWidth / 2, ctx.margin.top - ctx.strokeWidth / 2);
-
+  ctx.translate(config.margin.left - config.lineWidth / 2, config.margin.top - config.lineWidth / 2);
   ctx.strokeRect(0, 0, w, h);
+
   ctx.setLineDash([1, 3]);
   ctx.lineDashOffset = [0, 0];
   ctx.lineCap = 'butt';
@@ -108,7 +112,7 @@ function updateContactMap(ctx, data, required, pos={x: 0, y: 0}) {
   // ctx.save();
   for (let i = 0; i < data.x.length; i++) {
     ctx.save();
-    if (flag && i === m) {
+    if (ctx.highlighted && i === m) {
       ctx.fillStyle = config.textHighlightColor;
       ctx.strokeStyle = config.lineHighlightColor;
     } else {
@@ -126,7 +130,7 @@ function updateContactMap(ctx, data, required, pos={x: 0, y: 0}) {
     ctx.restore();
   }
   for (let i = 0; i < data.y.length; i++) {
-    if (flag && i === n) {
+    if (ctx.highlighted && i === n) {
       ctx.fillStyle = config.textHighlightColor;
       ctx.strokeStyle = config.lineHighlightColor;
     } else {
@@ -145,7 +149,7 @@ function updateContactMap(ctx, data, required, pos={x: 0, y: 0}) {
     for (let j = 0; j < data.y.length; j++) {
       let obj = data.data[`${data.x[i]}-${data.y[j]}`];
       if (obj.value) {
-        if (flag && i === m && j === n) {
+        if (ctx.highlighted && i === m && j === n) {
           ctx.fillStyle = getColor(obj.type, true);
           ctx.shadowColor = ctx.fillStyle;
           ctx.shadowBlur = 8;
@@ -153,7 +157,7 @@ function updateContactMap(ctx, data, required, pos={x: 0, y: 0}) {
           ctx.fillStyle = getColor(obj.type, false);
         }
         ctx.beginPath();
-        ctx.arc( (i + 1) * gridWidth, (j + 1) * gridWidth, config.circleRadius,0, Math.PI * 2);
+        ctx.arc((i + 1) * gridWidth, (j + 1) * gridWidth, config.circleRadius, 0, Math.PI * 2);
         ctx.fill();
         ctx.shadowBlur = 0;
       }
@@ -163,22 +167,22 @@ function updateContactMap(ctx, data, required, pos={x: 0, y: 0}) {
   ctx.restore();
 }
 
-function getIndex(x, max, unit, r) {
-  if (x > max - unit + r) {
+function getIndex(d, max, unit, r) {
+  if (d < (unit - r) || d > (max - unit + r)) {
     return -1;
   }
-  let x1 = x - x % unit;
+  let x1 = d - d % unit;
   let x2 = x1 + unit;
-  if (x - x1 < r) {
+  if (d - x1 < r) {
     return x1 / unit - 1;
   }
-  if (x2 - x < r) {
-    return x2 / unit -1;
+  if (x2 - d < r) {
+    return x2 / unit - 1;
   }
   return -1;
 }
 
-function updateInfoPanel(panel, obj, pos={top: 0, left: 0}) {
+function updateInfoPanel(panel, obj, pos = {top: 0, left: 0}) {
   panel.style.top = `${pos.top}px`;
   panel.style.left = `${pos.left}px`;
   let spans = panel.getElementsByClassName('value');
@@ -186,6 +190,5 @@ function updateInfoPanel(panel, obj, pos={top: 0, left: 0}) {
   let i = 0;
   for (let span of spans) {
     span.innerText = `${values[i++]}`;
-    console.log(span);
   }
 }
